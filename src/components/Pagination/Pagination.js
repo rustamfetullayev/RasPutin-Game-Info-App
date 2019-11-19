6b87ad16706354ac65a2b1ab4641ec4c78bba33d
CimbomRusu <rustamfetullayev123@gmail.com>
2019-11-19 15:40:01 +0400

    pagination updated

--- src/components/Pagination/Pagination.js
+++ src/components/Pagination/Pagination.js
@@ -1,10 +1,11 @@
 import React, { Component } from "react";
+import './Pagination.css';
 
 export default class Pagination extends Component {
   render() {
     let pages = [];
     let currentPage = this.props.page;
-    let pageCount = this.props.count / 20;
+    let pageCount = Math.floor(this.props.count / 20);
     console.log(pageCount);
 
     if (currentPage + 4 <= 9) {
@@ -55,9 +56,17 @@ export default class Pagination extends Component {
     return (
       <div className="container">
         <div className="row pt-5 pb-5">
-          <div className="col-md-6 offset-md-3">
+          <div className="col-md-8 offset-md-2">
             <div className="pagination">
-              <ul className="pag-list">{pages}</ul>
+              <ul className="pag-list">
+                <li>
+                  <span className='pag-item jump' onClick={() => this.props.loadComponent(1)}>first page</span>
+                </li>
+                {pages}
+                <li>
+                  <span className='pag-item jump' onClick={() => this.props.loadComponent(pageCount)}>last page</span>
+                </li>
+              </ul>
             </div>
           </div>
         </div>
